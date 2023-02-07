a9cce8112c18dbacdf964ecfba8e647f36e638f0
rlatjdrud <jkkim1933@naver.com>
2023-02-07 14:25:47 +0900

    linear_stack_version2

--- main.cpp
+++ main.cpp
@@ -27,18 +27,59 @@ void push(element item){
 
 }
 
-void printstack(){
-	while(top->link!=NULL){
-	printf("data:%d\n",top->data);	
-	top=top->link;
-}
-	printf("data:%d\n",top->data);	
+void del(){
+	if(top==NULL) {printf("stack is empty!\n");}
+	else {
+		stack* temp;	
+		temp=top;
+		top=top->link;
+		delete temp;
+	}
 	
 }
 
+element pop(){
+	
+	if(top==NULL) {printf("stack is empty!\n");
+	return -1;}
+	
+	else{
+	element a = top->data;
+	top=top->link;
+	return a;
+	}
+}
+
+element peek(){
+	
+	if(top==NULL) {printf("stack is empty!\n"); return -1;}
+	else {
+	return top->data;
+	}
+}
+
+void printstack(){
+	stack* p=top;
+	while(p){
+	printf("data:%d\n",p->data);	
+	p=p->link;
+	
+}
+}
+
+
 int main(){
 	push(1);
- 	printstack();
+	push(2);
+	push(3);
+	printstack();
+	del();
+	printstack();
+	element a = pop();
+	printf("%d\n",a);
+	printstack();
+	a=peek();
+	printf("%d\n",a);
 return 0;
 
 }
