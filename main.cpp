d077cc60651365139f7f802a946c94619b73b08c
rlatjdrud <jkkim1933@naver.com>
2023-02-07 16:00:07 +0900

    linear_stack_Version3

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef int element;
+typedef char element;
 
 class stack{
 public : 
@@ -67,19 +67,49 @@ void printstack(){
 }
 }
 
+int testpair(char* exp){
+char symbol;
+char open_pair;
+int i,length=strlen(exp);
+top=NULL;
+
+for(int i=0; i<length; i++){
+	symbol=exp[i];
+	switch(symbol){
+		case '(' : 
+		case '[' :
+		case '{' :
+	push(symbol); break;
+
+		case ')' : 
+		case ']' : 
+		case '}' :
+	if(top==NULL) return 0;
+	else {
+		open_pair=pop();
+		if((open_pair=='(' && symbol!=')') ||
+		  (open_pair=='[' && symbol!=']') ||
+		  (open_pair=='{' && symbol!='}'))
+		return 0;
+
+		else break;	
+	}
+	
+	}	
+}
+
+if(top==NULL) return 1;
+else return 0;
+
+}
 
 int main(){
-	push(1);
-	push(2);
-	push(3);
-	printstack();
-	del();
-	printstack();
-	element a = pop();
-	printf("%d\n",a);
-	printstack();
-	a=peek();
-	printf("%d\n",a);
+	char* express="((A+B)-3)*5+[{COS(X+Y)+7}-1]*4";
+        printf("%s",express);
+	if(testpair(express)==1) printf("올바른 괄호\n");
+	else printf("잘못된 괄호\n");	
+	getchar();
+
 return 0;
 
 }
